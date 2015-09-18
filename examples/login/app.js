50beb4f423b01b74a3d9bc73d36ac8fe18146a69
Daniel Warner <danywarner@gmail.com>
2015-09-18 10:54:19 -0500

    personalized for logs

--- examples/login/app.js
+++ examples/login/app.js
@@ -2,14 +2,11 @@ var express = require('express')
   , passport = require('passport')
   , util = require('util')
   , FacebookStrategy = require('passport-facebook').Strategy
-  , logger = require('morgan')
-  , session = require('express-session')
-  , bodyParser = require("body-parser")
   , cookieParser = require("cookie-parser")
   , methodOverride = require('method-override');
 
-var FACEBOOK_APP_ID = "--insert-facebook-app-id-here--"
-var FACEBOOK_APP_SECRET = "--insert-facebook-app-secret-here--";
+var FACEBOOK_APP_ID = '754657637972313';
+var FACEBOOK_APP_SECRET = 'e4a2dc4d8b2af80389ec981c4f90945b';
 
 
 // Passport session setup.
@@ -33,12 +30,17 @@ passport.deserializeUser(function(obj, done) {
 //   credentials (in this case, an accessToken, refreshToken, and Facebook
 //   profile), and invoke a callback with a user object.
 passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
+    clientID: '754657637972313',
+    clientSecret: 'e4a2dc4d8b2af80389ec981c4f90945b',
     callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
+  function(req, accessToken, refreshToken, profile, done) {
+if (req.user) {
+  console.log('USUARIO RETORNADOOOOOOOOO'+req.user);
+}
+  else{
+    console.log(profile.id+' '+profile.displayName+' '+profile._json);
+  }
     process.nextTick(function () {
       
       // To keep the example simple, the user's Facebook profile is returned to
@@ -58,11 +60,8 @@ var app = express();
 // configure Express
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.use(logger());
   app.use(cookieParser());
-  app.use(bodyParser());
   app.use(methodOverride());
-  app.use(session({ secret: 'keyboard cat' }));
   // Initialize Passport!  Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
   app.use(passport.initialize());
@@ -71,8 +70,10 @@ var app = express();
 
 
 app.get('/', function(req, res){
+    //console.log(req);
   res.render('index', { user: req.user });
 });
+
 
 app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', { user: req.user });
@@ -82,28 +83,10 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/facebook
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Facebook authentication will involve
-//   redirecting the user to facebook.com.  After authorization, Facebook will
-//   redirect the user back to this application at /auth/facebook/callback
-app.get('/auth/facebook',
-  passport.authenticate('facebook'),
-  function(req, res){
-    // The request will be redirected to Facebook for authentication, so this
-    // function will not be called.
-  });
-
-// GET /auth/facebook/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 
 app.get('/logout', function(req, res){
   req.logout();
